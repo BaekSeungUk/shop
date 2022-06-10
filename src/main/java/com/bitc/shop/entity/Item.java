07fa5ed5b1da9d55c0cd2d6cdb5df040f9522082
DESKTOP-PL0OCAR\user <dnrtmd09@naver.com>
2022-06-10 15:42:53 +0900

    0610수업

--- src/main/java/com/bitc/shop/entity/Item.java
+++ src/main/java/com/bitc/shop/entity/Item.java
@@ -1,6 +1,7 @@
 package com.bitc.shop.entity;
 
 import com.bitc.shop.constant.ItemSellStatus;
+import com.bitc.shop.dto.ItemFormDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -35,7 +36,15 @@ public class Item extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus;
 
-   /* private LocalDateTime regTime;
+    public void updateItem(ItemFormDto itemFormDto) {
+        this.itemNm = itemFormDto.getItemNm();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
+
+    /* private LocalDateTime regTime;
 
     private LocalDateTime updateTime;*/
 
